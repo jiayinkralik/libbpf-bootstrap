d2336e8b6abcb42f34e3498b4c204943ea1ed342
Jiayin <jkralik@student.ubc.ca>
2025-05-30 00:50:24 -0700

    made a simple alu & arsh program out of minimal

--- examples/c/minimal.bpf.c
+++ examples/c/minimal.bpf.c
@@ -10,12 +10,23 @@ int my_pid = 0;
 SEC("tp/syscalls/sys_enter_write")
 int handle_tp(void *ctx)
 {
-	int pid = bpf_get_current_pid_tgid() >> 32;
+	// int pid = bpf_get_current_pid_tgid() >> 32;
 
-	if (pid != my_pid)
-		return 0;
+	// if (pid != my_pid)
+	// 	return 0;
 
-	bpf_printk("BPF triggered from PID %d.\n", pid);
+	// bpf_printk("BPF triggered from PID %d.\n", pid);
 
-	return 0;
+	// return 0;
+
+	// a simple ALU64 operation
+	volatile __u64 a = 100;
+    volatile __u64 b = 200;
+    __u64 sum = a + b;
+
+	// triggering arsh
+	volatile __s64 val = -1024;  // signed 64-bit integer
+    val = val >> 3;             // this triggers ARSH
+
+    return sum; 
 }
